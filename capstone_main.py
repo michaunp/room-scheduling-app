df38c49905360072319e03921a340fb73a914ba3
Michaun Pierre <michaun.pierre@bison.howard.edu>
2019-10-17 01:14:33 +0000

    Added heroku credentials to app

--- capstone_main.py
+++ capstone_main.py
@@ -6,18 +6,21 @@ from flask import Flask, flash, redirect, render_template, \
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-
-config = {
-"apiKey": "",
-"authDomain": "",
-"databaseURL": "",
-"projectId": "",
-"storageBucket": "",
-"messagingSenderId": "",
-"appId": ""
+#Your web app's Firebase configuration
+var firebaseConfig = {
+	"apiKey": "AIzaSyCJtfb68rShfsaknLUPOANCcLuVmsnV3PI",
+	"authDomain": "fir-app-th4.firebaseapp.com",
+	"databaseURL": "https://fir-app-th4.firebaseio.com",
+	"projectId": "fir-app-th4",
+	"storageBucket": "fir-app-th4.appspot.com",
+	"messagingSenderId": "130663563400",
+	"appId": "1:130663563400:web:4e67967055c03f6c4d5f3d",
+	"measurementId": "G-X6FNZLK0YW"
 }
-
-firebase = pyrebase.initialize_app(config)
+	
+#Initialize Firebase
+firebase = pyrebase.initialize_app(firebaseConfig)
+firebase.analytics()
 
 auth = firebase.auth()
 
